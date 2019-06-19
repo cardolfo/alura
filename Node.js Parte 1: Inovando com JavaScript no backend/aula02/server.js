be7c5404ec2cd544a4211ca6321d86f62b6e10b6
cardolfo <cardolfo@gmail.com>
2019-06-19 19:31:37 -0300

    Fazendo aula

--- Node.js Parte 1: Inovando com JavaScript no backend/aula02/server.js
+++ Node.js Parte 1: Inovando com JavaScript no backend/aula02/server.js
@@ -1,4 +1,38 @@
-const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, function () {
+    console.log('Servidor rodando na porta 3000');
+})
+
+app.get('/', function (request, response) {
+    response.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+        </html>
+    `);
+});
+
+app.get('/livros', function (request, response) {
+    response.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Lista de Livros </h1>
+            </body> 
+        </html>
+    `);
+});
+
+/*const http = require('http');
 
 const server = http.createServer(function (request, response) {
 
@@ -9,9 +43,8 @@ const server = http.createServer(function (request, response) {
     }else if (request.url == '/livros') {
         html = '/livros';
     }
-    
+
     response.end(html);
 });
 
-server.listen(3000);
-
+server.listen(3000);*/
